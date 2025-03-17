72706a3bba2b7d672ce7581c8a364b516f961169
Adam 2 <111005987+RafaelAdamRamos@users.noreply.github.com>
2025-03-17 07:16:28 -0300

    Delete Cadastro de produtos/src/Eletronicos.java

--- Cadastro de produtos/src/Eletronicos.java
+++
@@ -1,9 +0,0 @@
-public class Eletronicos {
-    String marcaEletronico = " ";
-    int garantiaEletronico = 0;
-
-    public Eletronicos(String marcaEletronico, int garantiaEletronico) {
-
-    }
-
-}
